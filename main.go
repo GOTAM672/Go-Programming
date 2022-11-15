d78a0b97506d79a4d494d98c8eeb5113eb815c7c
Gotam Gorabh <gautamy672@gmail.com>
2022-11-15 21:15:56 +0530

    Initial Learning

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"  //  import formate
+
+
+ // var print = fmt.println
+
+
+func main() {
+  fmt.Println("Hello world! ")
+  fmt.Println("My name is Gotam Gorabh")
+
+  // variables
+  var a int   /* method 1 */
+  a = 42
+  var b int = 30 /* method 2 */
+  c := 32        /* method 3 */
+  fmt.Println(a)
+  fmt.Println(b)
+  fmt.Println(c)
+
+}
