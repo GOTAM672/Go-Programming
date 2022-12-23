d6f4736d83c66f8ca0f1ed1edeb38498be2ec7f7
Gotam Gorabh <gautamy672@gmail.com>
2022-12-23 08:36:37 +0530

    Day 1.1 : finished Go variables

--- code/variable.go
+++ code/variable.go
@@ -1,14 +1,29 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "strconv"
+)
 
 /* At package Level */
-l := 34  // we can not declare it.
+l := 34  // we can not declare it. Gives Error
 
+/* we can also crrate variable in block at a time */
+
+var (
+  Name string = "Gotam Gorabh"
+  SchoolName string = IIIT
+  Age int = 23
+)
+
+/*
+   lower case at package level - varible are scoped to the packsge
+   upper case at package level - variable are globally visible
+*/
 
 func main() {
 
-  /* format 1 */
+  /* format one */
   var i int       // Declaration
   i = 30          // initialisation
 
@@ -18,8 +33,16 @@ func main() {
 /* Format three*/
   Age := 23
 
-  fmt.Printf("%v, %T", i, i)
+  /* Important */
+  var Id int = 173
+  Id := 200       // This gives error because we redeclaring it twice.
+  fmt.Printf("%v, %T", Age, Age)
 
-
+  /* covertion variable type */
+  var k int =23
+  var l float32
+  var str string
+  l = float32(k)
+  str = strconv.Itoa(k)
 
 }
