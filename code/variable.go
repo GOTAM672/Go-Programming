78a5034db0978a6c5ecc562854a70f876f628380
Gotam Gorabh <gautamy672@gmail.com>
2022-12-23 07:48:41 +0530

    Day 1.0 : Go variables

---
+++ code/variable.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+/* At package Level */
+l := 34  // we can not declare it.
+
+
+func main() {
+
+  /* format 1 */
+  var i int       // Declaration
+  i = 30          // initialisation
+
+  /* Format two */
+  var j int = 1947    // Declaration + initialisation
+
+/* Format three*/
+  Age := 23
+
+  fmt.Printf("%v, %T", i, i)
+
+
+
+}
